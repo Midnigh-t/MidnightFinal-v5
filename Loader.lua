59f0897f8152abaa77ce1442f33f36d64fff3457
Midnigh-t <93854580+Midnigh-t@users.noreply.github.com>
2021-11-07 19:26:14 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,76 @@
-print("a")
+local lib = {}
+local RunService = game:GetService("RunService")
+
+local colors = {
+    ["Black"] = {"@@BLACK@@","Black"},
+    ["Blue"] = {"@@BLUE@@","Blue"},
+    ["Green"] = {"@@GREEN@@","Green"},
+    ["Cyan"] = {"@@CYAN@@","Cyan"},
+    ["Red"] = {"@@RED@@","Red"},
+    ["Magenta"] = {"@@MAGENTA@@","Magenta"},
+    ["Brown"] = {"@@BROWN@@","Brown"},
+    ["Light Gray"] = {"@@LIGHT_GRAY@@","Light Gray"},
+    ["Dark Gray"] = {"@@DARK_GRAY@@","Dark Gray"},
+    ["Light Blue"] = {"@@LIGHT_BLUE@@","Light Blue"},
+    ["Light Green"] = {"@@LIGHT_GREEN@@","Light Green"},
+    ["Light Cyan"] = {"@@LIGHT_CYAN@@","Light Cyan"},
+    ["Light Red"] = {"@@LIGHT_RED@@","Light Red"},
+    ["Light Magenta"] = {"@@LIGHT_MAGENTA@@","Light Magenta"},
+    ["Yellow"] = {"@@YELLOW@@","Yellow"},
+    ["White"] = {"@@WHITE@@","White"}
+
+
+}
+
+function lib:add(name)
+    rconsolename(name)
+end
+
+function lib:message(msg)
+    rconsoleprint(msg.."\n")
+end
+
+function lib:makeSpace()
+    rconsoleprint("\n")
+end
+
+function lib:infoMessage(msg)
+    rconsoleinfo(msg)
+end
+
+function lib:warnMessage(msg)
+    rconsolewarn(msg)
+end
+
+function lib:errorMessage(msg)
+    rconsoleerr(msg)
+end
+
+function lib:setColor(color)
+    local canContinue = false
+    local colorSelected = ""
+
+    for i,v in pairs(colors) do
+        if color == v[2] then
+            canContinue = true
+            colorSelected = v[1]
+            break
+        end
+    end
+
+    if canContinue and colorSelected ~= "" then
+        rconsoleprint(colorSelected)
+    end
+
+end
+
+function lib:addInput(trigger,callback)
+        while wait() do
+        local input = rconsoleinput(input)
+        if trigger == input then
+            callback()
+        end
+    end
+end
+
+return lib
